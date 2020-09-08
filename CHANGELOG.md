5868243b0067e84b5cf380d8d61f70bb494c0331
Scott Reilly <scott@coffee2code.com>
2020-09-07 23:59:03 -0700

    Release v1.8.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## _(in-progress)_
+## 1.8.1 _(2020-09-07)_
 * Change: Restructure unit test file structure
     * New: Create new subdirectory `phpunit/` to house all files related to unit testing
     * Change: Move `bin/` to `phpunit/bin/`
--- disable-search.php
+++ disable-search.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Disable Search
- * Version:     1.8
+ * Version:     1.8.1
  * Plugin URI:  https://coffee2code.com/wp-plugins/disable-search/
  * Author:      Scott Reilly
  * Author URI:  https://coffee2code.com/
@@ -18,7 +18,7 @@
  *
  * @package Disable_Search
  * @author  Scott Reilly
- * @version 1.8
+ * @version 1.8.1
  */
 
 /*
@@ -51,7 +51,7 @@ class c2c_DisableSearch {
 	 * @since 1.3
 	 */
 	public static function version() {
-		return '1.8';
+		return '1.8.1';
 	}
 
 	/**
